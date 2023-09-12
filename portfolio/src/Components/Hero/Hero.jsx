c8c6df73adf9f38d3a2281c5b102c01af9c00b2c
NjokiKimani <maninjoki@gmail.com>
2023-09-12 13:30:50 -0700

    Added framer motion

--- portfolio/src/Components/Hero/Hero.jsx
+++ portfolio/src/Components/Hero/Hero.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import './Hero.css'
+import {TypeAnimation} from 'react-type-animation'
+import {motion} from 'framer-motion'
 
 function Hero() {
   return (
     <section id="intro">
       <div className="introContent">
         <span className="hello">Hello, </span>
-        <span className="introText">I'm <span className='name'>Kimani</span>
-       <br />
-       I am a front-end developer
-       <p className="introParagraph">
-        I am passionate about user-centered web developnment, and understand its potential within dynamic digital spaces.
-       </p>
+        <span className="introText">
+          I'm <span className="name">Kimani</span>
+          <br />
+          <span className="jobTitle">a </span>
+         <TypeAnimation sequence = {[
+            'front-end developer',  
+             'financial analyst',  
+              'management consultant',  
+         ]} speed={50} wrapper='span' repeat={Infinity}
+         
+         />
+         
+          <p className="introParagraph">
+            I am passionate about user-centered web development, and understand
+            its potential within dynamic digital spaces.
+          </p>
         </span>
       </div>
       <img src="" alt="" />
