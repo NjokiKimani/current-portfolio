21bd9942185892c7cb00ec0706acc0816c2b24ee
NjokiKimani <maninjoki@gmail.com>
2023-09-13 09:42:43 -0700

    Added projects section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
+import Projects from "./Components/Projects/Projects"
 function App() {
   return (
     <div className="App">
 <Navbar />
 <Hero />
+<Projects />
     </div>
   );
 }
---
+++ portfolio/src/Components/Projects/Projects.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Projects.css'
+
+function Projects() {
+  return (
+    <section id="projects">
+      <h2 className="projectsTitle">Select Works</h2>
+      <span className="projectsDescription">
+        I have a strong foundation in converting design into pixel-perfect code.
+        I am passionate about user-centered web development, and understand its
+        potential within dynamic digital spaces. I really enjoy solving problems
+        with clean scalable solutions.
+      </span>
+      <div className="projectImages">
+        <img src="" alt="" className="projectImage" />
+        <img src="" alt="" className="projectImage" />{" "}
+        <img src="" alt="" className="projectImage" />{" "}
+        <img src="" alt="" className="projectImage" />{" "}
+        <img src="" alt="" className="projectImage" />{" "}
+        <img src="" alt="" className="projectImage" />
+      </div>
+    </section>
+  );
+}
+
+export default Projects
